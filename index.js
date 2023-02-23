3aa9093f90c13d044f6d8a348d398c8ecd7f475e
John Ipson <john@devpipeline.com>
2023-02-23 16:31:20 -0700

    Homework instructions added

---
+++ index.js
@@ -0,0 +1,15 @@
+// Homework 2/23/2023 //
+
+// 1) Given an array, return a new array where the first and last elements from the original array are removed.
+// I want this done in a function.
+// If there's less than 3 elements, it shouldn't remove the items.
+
+// 2) Build a heading generator.
+// IE:
+// heading = 6
+// content = "hey there, I'm a header"
+// => '<h6>hey there, I'm a header</h6>'
+// Keep in mind, valid heading tags are only 1-6
+
+// 3) Swap the first and last characters of a string.
+// IE: "ryan" => "nyar"
